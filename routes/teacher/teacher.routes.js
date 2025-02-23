fd478c04ac3972d7bf3e6b52121febcd3a88be5e
Sandesh4141 <hackersp41@gmail.com>
2025-02-24 00:03:04 +0530

    added teacher routes for updating teacher profile

--- routes/teacher/teacher.routes.js
+++ routes/teacher/teacher.routes.js
@@ -1,11 +1,24 @@
 // teacher.routes.js
 import { Router } from "express";
-import { getTeacherDashboard } from "../../controllers/teacher.controller.js";
-
+import {
+  changePassword,
+  fetchTeacherProfile,
+  getTeacherDashboard,
+  updateProfile,
+  uploadProfilePicture,
+} from "../../controllers/teacher.controller.js";
+import { upload } from "../../middlewares/uploadProfilePic.middleware.js";
 const router = Router();
 
 // When a GET request is made to /teacher/dashboard, the dashboard data is returned.
 router.get("/dashboard", getTeacherDashboard);
-
+router.get("/profile", fetchTeacherProfile);
+router.put("/profile/update", updateProfile);
+router.post(
+  "/profile/upload",
+  upload.single("profilePicture"),
+  uploadProfilePicture
+);
+router.put("/profile/change-password", changePassword);
 
 export default router;
